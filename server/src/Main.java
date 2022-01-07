c4035836f88b39090e7e660eb93a42965f5ffffc
kyle65463 <kyle65463@gmail.com>
2022-01-07 20:05:52 +0800

    Add maven and firebase dependencies

--- server/src/Main.java
+++ server/src/Main.java
@@ -1,14 +1,39 @@
-import database.Firestore;
+import com.google.api.core.ApiFuture;
+import com.google.common.base.Supplier;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         if (args.length != 1) {
             System.out.println("usage: ./server <port>");
             return;
         }
-        int port = Integer.parseInt(args[0]);
-        Firestore firestore = new Firestore();
-        firestore.getDoc();
-        Server.run(port);
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(credentials)
+                .build();
+        FirebaseApp.initializeApp(options);
+        Firestore db = FirestoreClient.getFirestore();
+        ApiFuture<QuerySnapshot> query = db.collection("users").get();
+        QuerySnapshot querySnapshot = query.get();
+        List<QueryDocumentSnapshot> documents = querySnapshot.getDocuments();
+        for (QueryDocumentSnapshot document : documents) {
+            System.out.println("User: " + document.getId());
+            System.out.println(document);
+        }
+//        Server.run(port);
     }
 }
