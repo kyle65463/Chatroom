3c34c2e94e5742ceaf4a0292be0947e6f6128777
kyle65463 <kyle65463@gmail.com>
2022-01-13 22:03:24 +0800

    Add web socket

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Manager, Socket } from "socket.io-client";
 
 function Home() {
+	const [socket, setSocket] = useState<Socket>();
+
+	const initSocket = () => {
+		const manager = new Manager("localhost:9998");
+		const socket = manager.socket("/");
+		socket.onAny((message) => {});
+		setSocket(socket);
+	};
+
 	return (
 		<div>
-			HII	
+			<div className='btn primary' onClick={initSocket}>
+				connect
+			</div>
 		</div>
-	)
+	);
 }
 
-export default Home
+export default Home;
