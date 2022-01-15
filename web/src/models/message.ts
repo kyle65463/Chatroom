ecd36dfc5c89f4a8d4fd90cecbc791b37734bf60
kyle65463 <kyle65463@gmail.com>
2022-01-15 19:36:03 +0800

    Refactore parser

--- web/src/models/message.ts
+++ web/src/models/message.ts
@@ -1,62 +1,68 @@
+interface Header {
+	key: string;
+	value: string;
+}
+
 export abstract class Message {
-	public static loginParse(data: string): Message {
-		let index: number = data.indexOf("OK");
-		if (index == -1) {
-			console.log("login failed");
-			return new LoginFailedMessage();
-		}
-		let authIndex: number = data.indexOf("authToken");
-		let authToken: string = "";
-		for (let i: number = authIndex + 12; i < data.length; i++) {
-			if (data[i] == '"') {
-				break;
-			}
-			authToken += data[i];
-		}
-		console.log(authToken);
-		return new LoginSuccessMessage(authToken);
+	constructor(header: Header[], body: any) {
+		this.header = header;
+		this.body = body;
 	}
+
+	public header: Header[];
+	public body: any;
 
 	public static parse(message: MessageEvent<any>): Message {
 		const lines = (message.data as string).split("\r\n");
 		const bodyIndex = lines.findIndex((e) => e == "") + 1;
-		const body = lines[bodyIndex];
+		const rawBody = lines[bodyIndex];
+		const body = JSON.parse(rawBody);
 		const status = parseInt(lines[0].split(" ")[1]);
-		const headers = lines.slice(1, bodyIndex - 1).map((line) => line.split(": ", 2));
+		const headers = lines
+			.slice(1, bodyIndex - 1)
+			.map((line) => ({ key: line.split(": ", 2)[0], value: line.split(": ", 2)[1] }));
+		const path = headers.find((e) => e.key === "Path")?.value;
+		if (!path) return new ErrorMessage();
 
-		let data: string = message.data;
-		let path_Index: number = data.indexOf("Path: /");
-		let path: string = "";
-		for (let i: number = path_Index + 7; i < data.length; i++) {
-			if (data[i] == "\r") {
-				break;
+		if (status === 200) {
+			// Success
+			if (path === "/login") {
+				return new LoginSuccessMessage(headers, body);
 			}
-			path += data[i];
+			if (path === "/register") {
+			}
+			if (path === "/friend") {
+			}
+		} else {
+			// Fail
 		}
-		if (path == "login") {
-			return Message.loginParse(data);
-		}
-		console.log(path);
-		let line: string;
-		return new ListFriendMessage(["luca"]);
+		return new ErrorMessage();
 	}
 }
 
 export class LoginSuccessMessage extends Message {
-	constructor(authToken: string) {
-		super();
-		this.authToken = authToken;
+	constructor(header: Header[], body: any) {
+		super(header, body);
+		this.authToken = body.authToken;
 	}
+
 	public authToken: string;
 }
 
-export class LoginFailedMessage extends Message {}
+export class LoginFailedMessage extends Message {
+	constructor(header: Header[], body: any) {
+		super(header, body);
+	}
+}
 
 export class ListFriendMessage extends Message {
-	constructor(friends: string[]) {
-		super();
-		this.friends = friends;
+	constructor(header: Header[], body: any) {
+		super(header, body);
 	}
+}
 
-	public friends: string[] = [];
+export class ErrorMessage extends Message {
+	constructor() {
+		super([], {});
+	}
 }
