c1f827fa6510e5f15b8a62d904e4bcec567c2848
kyle65463 <kyle65463@gmail.com>
2022-01-16 10:59:54 +0800

    Restyle friend list

--- web/src/pages/friend.tsx
+++ web/src/pages/friend.tsx
@@ -1,115 +1,107 @@
 import Link from "next/link";
 import React, { useContext, useEffect, useState } from "react";
 import { SocketContext } from "../context/context";
-import { AddFriendFailedMessage, AddFriendSuccessMessage, DeleteFriendFailedMessage, DeleteFriendSuccessMessage, ListFriendMessage, Message, NullMessage } from "../models/message";
-import {Friend} from "../models/user";
+import {
+	AddFriendFailedMessage,
+	AddFriendSuccessMessage,
+	DeleteFriendFailedMessage,
+	DeleteFriendSuccessMessage,
+	ListFriendMessage,
+} from "../models/message";
+import { Friend } from "../models/user";
 
 function Friend() {
-	const { socket, authToken ,message, setMessage, user } = useContext(SocketContext);
-    // socket?.send(friend);
-    let nullfriend:Friend[] = [];
-    const [friendList, setfriendList] = useState(nullfriend);
-    const [addfriend, setAddFriend] = useState("");
-	const [deletefriend, setDeleteFriend] = useState("");
+	const { socket, authToken, message, setMessage, user } = useContext(SocketContext);
+	// socket?.send(friend);
+	let nullfriend: Friend[] = [];
+	const [friendList, setfriendList] = useState(nullfriend);
+	const [friendUsername, setFriendUsername] = useState("");
 
-    const onAddFriendChange = (e: any) => {
-		setAddFriend(e.target.value);
+	const onFriendUsernameChange = (e: any) => {
+		setFriendUsername(e.target.value);
 	};
-    
-    const onDeleteriendChange = (e: any) => {
-		setDeleteFriend(e.target.value);
+
+	const ADD = () => {
+		const body: string = JSON.stringify({ username: friendUsername });
+		if (authToken) {
+			socket?.send("/friend/add", body, "POST", authToken);
+		}
 	};
-    
-    const ADD = () =>{
-        const body: string = JSON.stringify({username:addfriend});
-        if(authToken){
-            socket?.send("/friend/add",body,"POST",authToken);
-        }
-    }
 
-    const DELETE = () =>{
-        const body: string = JSON.stringify({username:deletefriend});
-        if(authToken){
-            socket?.send("/friend/delete",body,"POST",authToken);
-        }
-    }
-    
-
+	const DELETE = () => {
+		const body: string = JSON.stringify({ username: friendUsername });
+		if (authToken) {
+			socket?.send("/friend/delete", body, "POST", authToken);
+		}
+	};
 
 	useEffect(() => {
-        if(message instanceof ListFriendMessage){
-            let friends:Friend[] = message.friendList;
-            setfriendList(friends);
-            // let nullMessage:Message = new NullMessage([],"");
-            // setMessage(nullMessage);
-        }
-        if(message instanceof AddFriendSuccessMessage){
-            alert("Add Friend Success!");
-            if(authToken){
-                socket?.send("/friend/list","{}","POST",authToken);
-            }
-        }
-        if(message instanceof AddFriendFailedMessage){
-            alert("Add Friend Failed");
-        }
-        if(message instanceof DeleteFriendSuccessMessage){
-            alert("Delete Friend Success!");
-            if(authToken){
-                socket?.send("/friend/list","{}","POST",authToken);
-            }
-        }
-        if(message instanceof DeleteFriendFailedMessage){
-            alert("Delete Friend Failed");
-        }
-    }, [message]);
+		if (message instanceof ListFriendMessage) {
+			let friends: Friend[] = message.friendList;
+			setfriendList(friends);
+			// let nullMessage:Message = new NullMessage([],"");
+			// setMessage(nullMessage);
+		}
+		if (message instanceof AddFriendSuccessMessage) {
+			alert("Add Friend Success!");
+			if (authToken) {
+				socket?.send("/friend/list", "{}", "POST", authToken);
+			}
+		}
+		if (message instanceof AddFriendFailedMessage) {
+			alert("Add Friend Failed");
+		}
+		if (message instanceof DeleteFriendSuccessMessage) {
+			alert("Delete Friend Success!");
+			if (authToken) {
+				socket?.send("/friend/list", "{}", "POST", authToken);
+			}
+		}
+		if (message instanceof DeleteFriendFailedMessage) {
+			alert("Delete Friend Failed");
+		}
+	}, [message]);
 
-    useEffect(()=>{
-        if(authToken){
-            socket?.send("/friend/list","{}","POST",authToken);
-        }
-    },[])
-
+	useEffect(() => {
+		if (authToken) {
+			socket?.send("/friend/list", "{}", "POST", authToken);
+		}
+	}, []);
 
 	return (
-		<div className='h-screen'>
+		<div className='min-h-screen p-20 bg-base-200'>
 			<div className='flex flex-col items-center h-full'>
-				<p className="text-3xl m-10">Friend List</p>
-                <div className='flex justify-center'>
-                    {friendList.map((friend, i) => (
-                        <div className='bg-gray-100  p-4 mx-3'>
-                            {/* <p className='flex flex-col items-start'>friend {i}</p> */}
-                            <div className='flex flex-row items-start'>
-                                <p className='pr-2 py-1'>UserName: {friend.username}</p>
-                                {/* <p className='py-1'>DisplayName:{friend.displayName}</p> */}
-                            </div>
-                        </div>
-                    ))}
-			    </div>
-                <div>
-                    <div className="flex flex-col items-center m-5">
-                        <label htmlFor='username' className='label text-xl'>
-                            ADD:
-                        </label>
-                        <input id='addfriend' className='input input-bordered' type='text' onChange={onAddFriendChange}/>
-                    </div>
-                    <div className='m-10 btn btn-accent btn-wide' onClick={ADD}>
-                        ADD
-                    </div>
-                    <div className="flex flex-col items-center">
-                        <label htmlFor='username' className='label text-xl'>
-                            Delete:
-                        </label>
-                        <input id='password' className='input input-bordered' type='text' onChange={onDeleteriendChange}/>
-                    </div>
+				<p className='mb-10 text-3xl font-bold'>Friend List</p>
+				<div className='flex justify-center'>
+					{friendList.map(({ username }) => (
+						<div className='bg-white shadow-md w-60 card'>
+							<div className='overflow-hidden card-body'>
+								<p className='text-xl font-bold'>{username}</p>
+							</div>
+						</div>
+					))}
 				</div>
-				<div className='m-10 btn btn-accent btn-wide' onClick={DELETE}>
+				<div className='mt-10'>
+					<label htmlFor='username' className='text-xl label'>
+						Username:
+					</label>
+					<input
+						id='addfriend'
+						className='input input-bordered'
+						type='text'
+						onChange={onFriendUsernameChange}
+					/>
+				</div>
+				<div className='mt-5 btn btn-accent btn-wide' onClick={ADD}>
+					ADD
+				</div>
+				<div className='my-5 btn btn-accent btn-wide' onClick={DELETE}>
 					Delete
 				</div>
-                <Link href='/home'>
+				<Link href='/home'>
 					<div className='btn btn-wide'>BACK</div>
 				</Link>
 			</div>
-
 		</div>
 	);
 }
--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,41 +1,39 @@
 import Link from "next/link";
-import React, { useContext, useEffect, useReducer } from "react";
-import { SocketContext } from "../context/context";
 import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import { SocketContext } from "../context/context";
 import { Message, NullMessage } from "../models/message";
 
 function Home() {
-	const { socket, message , setMessage , user } = useContext(SocketContext);
+	const { socket, message, setMessage, user } = useContext(SocketContext);
 	const router = useRouter();
 	useEffect(() => {}, [message]);
 
-	const LOGOUT = () =>{
+	const LOGOUT = () => {
 		socket?.webSocket?.close();
-		let nullMessage:Message = new NullMessage([],"");
+		let nullMessage: Message = new NullMessage([], "");
 		setMessage(nullMessage);
 		router.push("/");
-	}
+	};
 
 	return (
-		<div className='h-screen'>
-			<div className='flex flex-col items-center justify-center h-full'>
-				<p className="text-3xl">{user?.username}'s personal Space</p>
-				<p className="text-xl m-10">Display Name: {user?.displayName}</p>
-				<p>Password: {user?.password}</p>
-				<div className='flex flex-col justify-center  m-20'>
+		<div className='min-h-screen bg-base-200'>
+			<div className='flex flex-col items-center justify-center h-full min-h-screen'>
+				<p className='text-4xl font-bold'>
+					<span className='text-accent'>{user?.username}</span>'s personal Space
+				</p>
+				<div className='flex flex-col justify-center m-20'>
 					<Link href='/friend'>
-							<div className='ml-6 mr-2 btn btn-accent btn-wide'>FRIEND</div>
+						<div className='ml-6 mr-2 btn btn-accent btn-wide'>FRIEND</div>
 					</Link>
 					<Link href='/chatmenu'>
-							<div className='m-10 ml-6 mr-2 btn btn-accent btn-wide'>CHAT MENU</div>
+						<div className='m-10 ml-6 mr-2 btn btn-accent btn-wide'>CHAT MENU</div>
 					</Link>
-
-					<div className='ml-6 mr-2 btn btn-accent btn-wide' onClick={LOGOUT}>LOG OUT</div>
-
+					<div className='ml-6 mr-2 btn btn-wide' onClick={LOGOUT}>
+						LOGOUT
+					</div>
 				</div>
 			</div>
-			
-			
 		</div>
 	);
 }
